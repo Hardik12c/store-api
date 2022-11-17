1e5305f6213d9d13f78c3f31565bc2e4e86644ed
Hardik Garg <93364270+Hardik12c@users.noreply.github.com>
2022-11-18 03:12:09 +0530

    working on fetching data from database according to user

--- routes/products.js
+++ routes/products.js
@@ -1,11 +1,12 @@
 const express=require('express')
-
+const Product=require('../models/product')
 const productrouter=express.Router();
 
 productrouter.route('/')
 .get(async(req,res)=>{
     try {
-        res.status(200).json({messg:"getting product"});
+        const products=await Product.find({});
+        res.status(200).json({products});
     } catch (error) {
         console.log(error);
     }
@@ -14,7 +15,8 @@ productrouter.route('/')
 productrouter.route('/static')
 .get(async(req,res)=>{
     try {
-        res.status(200).json({messg:"getting static product"});
+        const product=await Product.find({featured:true});
+        res.status(200).json({product});
     } catch (error) {
         console.log(error);
     }
