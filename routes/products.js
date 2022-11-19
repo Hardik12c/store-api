f904afde772443074b553f324972973855410b50
Hardik Garg <93364270+Hardik12c@users.noreply.github.com>
2022-11-20 02:53:19 +0530

    serving only user defined values and sorting them with the arguments passed by the users

--- routes/products.js
+++ routes/products.js
@@ -5,8 +5,27 @@ const productrouter=express.Router();
 productrouter.route('/')
 .get(async(req,res)=>{
     try {
-        const products=await Product.find({});
-        res.status(200).json({products});
+        const {name,featured,price,sort}=req.query; // it will take only those things which are present in api and drop other values
+        queryobj={};
+        if(featured){
+            queryobj.featured=(featured==='true')?true:false;
+        }
+        if(name){
+            queryobj.name=name;
+        }
+        if(price){
+            queryobj.price=price;
+        }
+        let results= Product.find(queryobj);
+        console.log(typeof(results));
+        if(sort){
+            const sortlist=sort.split(',').join(' ');
+            results=results.sort(sortlist)
+        }else{
+            results=results.sort('createdAt');
+        }
+        products=await results;  //waiting for results to be fetch
+        res.status(200).json({products,length:products.length});
     } catch (error) {
         console.log(error);
     }
